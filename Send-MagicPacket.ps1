018066ea2608127e623ec2b099e2bd20314ff5df
HerdHeaven <160198178+HerdHeaven@users.noreply.github.com>
2024-04-01 12:59:39 -0400

    Add files via upload

---
+++ Send-MagicPacket.ps1
@@ -0,0 +1,14 @@
+﻿param ([string]$macaddy=(Read-host -Prompt "Please Enter the MAC Address"))
+    $mymac = $macaddy.split(':') | %{ [byte]('0x' + $_) }
+    if ($mymac.Length -ne 6)
+    {
+        throw 'Mac Address Must be 6 hex Numbers Separated by : or -'
+    }
+    Write-Verbose "Creating UDP Packet"
+    $UDPclient = new-Object System.Net.Sockets.UdpClient
+    $UDPclient.Connect(([System.Net.IPAddress]::Broadcast),4000)
+    $packet = [byte[]](,0xFF * 6)
+    $packet += $mymac * 16
+    Write-Verbose ([bitconverter]::tostring($packet))
+    [void] $UDPclient.Send($packet, $packet.Length)
+    Write-Host  "   - Wake-On-Lan Packet of length $($packet.Length) sent to $mymac"
---
+++ Verify-HashValue.ps1
@@ -0,0 +1,43 @@
+﻿param ([PARAMETER(Mandatory=$FALSE,ValueFromPipeline=$FALSE)]
+[string]$FilePath,
+[PARAMETER(Mandatory=$FALSE,ValueFromPipeline=$FALSE)]
+[ValidateSet(“MACTripleDES”,”MD5”,”RIPEMD160”, "SHA1", "SHA256", "SHA384","SHA512")]
+[string]$Algorithm="SHA256")
+BEGIN {
+
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $InitialDirectory = "$env:USERPROFILE\Downloads"
+
+    function Get-FileName($InitialDirectory)
+    {
+        $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+        $OpenFileDialog.InitialDirectory = $InitialDirectory
+        $OpenFileDialog.ShowDialog() | Out-Null
+        $OpenFileDialog.FileName
+    }
+
+
+  }
+PROCESS{
+
+    If($FilePath -eq "")
+    {$FilePath = Get-FileName $InitialDirectory}
+    
+    $CalcHash = get-filehash -Path $($FilePath) -Algorithm SHA256
+
+    $Hash = Read-Host -Prompt "Please enter the $Algorithm Hash value to verify"
+
+    if($CalcHash.Hash -eq $Hash)
+    {Write-Output "The caluclated hash value matches!"}
+    else
+    {Write-Warning "The calculated has value does not match!`nProvided Hash:$hash`nCalculated Hash:$($calchash.hash)`n`n"}
+    
+  }
+END {
+    PAUSE
+    Clear-Variable InitialDirectory, hash, calchash
+}
+
+
+
